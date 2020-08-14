3a4b1e7f9f46b2881cad00c1ffa08f8f6850d52f
Brian Bartholomew <me@bcbrian.com>
2020-08-14 09:25:49 -0700

    waiting test

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-console.log("node");
-process.exit(1);
+console.log('node');
+let waiting = true;
+setTimeout(function waiting() {
+  waiting = false;
+  process.exit();
+}, 30000);
+
+function waiting() {
+  console.log('waiting');
+  if (waiting) {
+    setTimeout(waiting, 1000);
+  }
+}
+waiting();
