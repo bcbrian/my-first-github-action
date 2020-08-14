8af613ac1728b4803071913797c2b2f897c3a7d5
Brian Bartholomew <me@bcbrian.com>
2020-08-14 14:13:46 -0700

    trying to make a server

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ echo "Hello $1"
 echo "IP $2"
 time=$(date)
 echo "::set-output name=time::$time"
+npm i
 node index.js "$2"
--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-console.log('node');
+const express = require('express');
+const app = express();
+const port = 3000;
+
 const IP = process.argv[2];
-console.log(`IP ::> ${IP}`)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://${IP}:${port}`);
+});
+
 let waiting = true;
 setTimeout(function longWait() {
   console.log('done');
   waiting = false;
   process.exit();
-}, 30000);
+}, 60000);
 
 function shortWait() {
   console.log('waiting');
