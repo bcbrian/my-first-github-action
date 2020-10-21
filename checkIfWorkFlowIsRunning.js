56d77ac5d3742223271530fc44a1d4416c0f5676
Brian Bartholomew <me@bcbrian.com>
2020-10-21 12:08:58 -0700

    Update checkIfWorkFlowIsRunning.js

--- checkIfWorkFlowIsRunning.js
+++ checkIfWorkFlowIsRunning.js
@@ -2,13 +2,13 @@ const { Octokit } = require("@octokit/core");
 
 const octokit = new Octokit({ auth: process.env.GITTOKEN });
 async function main(){
-  const results = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
+  const resultQued = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
     owner: 'bcbrian',
     repo: 'my-first-github-action',
     status: 'queued',
   });
 
-  const result2 = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
+  const resultsRunning = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
     owner: 'bcbrian',
     repo: 'my-first-github-action',
     status: 'in_progress',
@@ -16,11 +16,11 @@ async function main(){
   console.log('****************');
   console.log('****************');
   console.log('****************');
-  console.log(results);
+  console.log(resultQued);
   console.log('****************');
   console.log('****************');
   console.log('****************');
-  console.log(results2);
+  console.log(resultsRunning);
   console.log('****************');
   console.log('****************');
   console.log('****************');
