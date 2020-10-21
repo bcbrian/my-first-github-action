27feb38270df92bcec570a2bb5fb6596565b4dcb
Brian Bartholomew <me@bcbrian.com>
2020-10-21 12:37:12 -0700

    Update checkIfWorkFlowIsRunning.js

--- checkIfWorkFlowIsRunning.js
+++ checkIfWorkFlowIsRunning.js
@@ -2,17 +2,20 @@ const { Octokit } = require("@octokit/core");
 
 const octokit = new Octokit({ auth: process.env.GITTOKEN });
 async function main(){
-  const resultQued = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
+  const resultQued = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs', {
     owner: 'bcbrian',
     repo: 'my-first-github-action',
+    workflow_id: 'manual-check-if-running.yml',
     status: 'queued',
   });
 
-  const resultsRunning = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
+  const resultsRunning = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs', {
     owner: 'bcbrian',
     repo: 'my-first-github-action',
+    workflow_id: 'manual-check-if-running.yml',
     status: 'in_progress',
   });
+  console.log('I am: ', process.argv[2]);
   console.log('****************');
   console.log('****************');
   console.log('****************');
