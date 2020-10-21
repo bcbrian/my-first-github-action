9225f46331ea179d3c8418fbe9cf4b3f7e7893b8
Brian Bartholomew <me@bcbrian.com>
2020-10-21 12:03:53 -0700

    Update checkIfWorkFlowIsRunning.js

--- checkIfWorkFlowIsRunning.js
+++ checkIfWorkFlowIsRunning.js
@@ -1,30 +1,32 @@
 const { Octokit } = require("@octokit/core");
 
 const octokit = new Octokit({ auth: process.env.GITTOKEN });
+async function main(){
+  const results = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
+    owner: 'bcbrian',
+    repo: 'my-first-github-action',
+    status: 'queued',
+  });
 
-const results = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
-  owner: 'bcbrian',
-  repo: 'my-first-github-action',
-  status: 'queued',
-});
+  const result2 = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
+    owner: 'bcbrian',
+    repo: 'my-first-github-action',
+    status: 'in_progress',
+  });
+  console.log('****************');
+  console.log('****************');
+  console.log('****************');
+  console.log(results);
+  console.log('****************');
+  console.log('****************');
+  console.log('****************');
+  console.log(results2);
+  console.log('****************');
+  console.log('****************');
+  console.log('****************');
 
-const result2 = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
-  owner: 'bcbrian',
-  repo: 'my-first-github-action',
-  status: 'in_progress',
-});
-console.log('****************');
-console.log('****************');
-console.log('****************');
-console.log(results);
-console.log('****************');
-console.log('****************');
-console.log('****************');
-console.log(results2);
-console.log('****************');
-console.log('****************');
-console.log('****************');
-
-setTimeout(() => {
-  process.exit();
-}, 3000);
+  setTimeout(() => {
+    process.exit();
+  }, 3000);
+}
+main();
