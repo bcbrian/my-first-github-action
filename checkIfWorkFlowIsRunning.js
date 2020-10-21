b74b1c267f5b09aebe951a061b5fa4a3e0ee1bac
Brian Bartholomew <me@bcbrian.com>
2020-10-21 11:20:09 -0700

    Update checkIfWorkFlowIsRunning.js

--- checkIfWorkFlowIsRunning.js
+++ checkIfWorkFlowIsRunning.js
@@ -1,8 +1,30 @@
 const { Octokit } = require("@octokit/core");
 
-const octokit = new Octokit({ auth: `personal-access-token123` });
+const octokit = new Octokit({ auth: process.env.GITTOKEN });
 
-await octokit.request('GET /repos/{owner}/{repo}/actions/runs', {
-  owner: 'octocat',
-  repo: 'hello-world'
-})
+const results = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
+  owner: 'bcbrian',
+  repo: 'my-first-github-action',
+  status: 'queued',
+});
+
+const result2 = await octokit.request('GET /repos/bcbrian/my-first-github-action/actions/runs', {
+  owner: 'bcbrian',
+  repo: 'my-first-github-action',
+  status: 'in_progress',
+});
+console.log('****************');
+console.log('****************');
+console.log('****************');
+console.log(results);
+console.log('****************');
+console.log('****************');
+console.log('****************');
+console.log(results2);
+console.log('****************');
+console.log('****************');
+console.log('****************');
+
+setTimeout(() => {
+  process.exit();
+}, 3000);
